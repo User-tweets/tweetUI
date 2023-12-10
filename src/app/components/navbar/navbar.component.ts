44b56c97a9a588e0d80329f55fe84d15edb94b86
Archit Gupta <architg564@gmail.com>
2023-12-10 22:36:51 +0530

    url

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginService } from '../../services/login.service';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
@@ -16,7 +16,11 @@ export class NavbarComponent implements OnInit {
 
   LoggedIn=false;
 
-  constructor(private loginService:LoginService, private router:Router) { }
+  constructor(private loginService:LoginService, private router:Router, private activatedRoute: ActivatedRoute) { 
+    activatedRoute.params.subscribe(val=>{
+      this.LoggedIn=this.loginService.isLoggedIn();
+    });
+  }
 
   ngOnInit(): void {
     this.LoggedIn=this.loginService.isLoggedIn();
@@ -24,7 +28,7 @@ export class NavbarComponent implements OnInit {
   
   logoutUser(){
     this.loginService.logout();
-    this.router.navigate(['/']);
+    this.router.navigate(['/tweetUI/']);
     // location.href = '/';
   }
 
